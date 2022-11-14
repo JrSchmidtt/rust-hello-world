1f7c85c651e3274f2da5fc1432c3da63e89d30de
jrschmidtt <junior.schmidt.99@gmail.com>
2022-11-14 01:48:58 -0300

    Var and const

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,21 @@
-fn main() {
-    let inteiro:i32 = 128;          // inteiro de 32 bytes
-    let decimal:f32 = 0.5;          // float de 32 bytes
-    let booleano:bool = true;       // booleano
-    let letra:char = 'a';           // caracter
+const PI:f32 = 3.1415;           // constante de tipo f32 escopo global
+static mut GLOBAL: i32 = 10;     // variável de tipo i32 escopo global pode ser mutavel porem é thread safe
 
+fn main() {
+    let inteiro:i32 = 128;       // inteiro de 32 bytes
+    let decimal:f32 = 0.5;       // float de 32 bytes
+    let booleano:bool = true;    // booleano
+    let letra:char = 'a';        // caracter
+
+    unsafe {
+        println!("varial global: {} tamanho {} bytes", GLOBAL, std::mem::size_of_val(&GLOBAL));
+    }
+
+    println!("PI: {} tamanho {} bytes", PI, std::mem::size_of_val(&PI));
     println!("inteiro: {} tamanho {} bytes", inteiro, std::mem::size_of_val(&inteiro));
     println!("decimal: {} tamanho {} bytes", decimal, std::mem::size_of_val(&decimal));
     println!("booleano: {} tamanho {} bytes", booleano, std::mem::size_of_val(&booleano));
     println!("letra: {} tamanho {} bytes", letra, std::mem::size_of_val(&letra));
 
-
-    println!("Hello, world!");
+    println!("Hello, world!");      // println! é uma macro não uma função	
 }
